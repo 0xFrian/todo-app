76778d78e86b947e32319c25d19fc7833e55336e
0xFrian <brianfong22@gmail.com>
2022-03-10 14:40:47 -0800

    Added components: FilterButton.js, Form.js, Todo.js, and UsePrevious.js

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function Form(props) {
+    const [name, setName] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault(); 
+        props.onSubmit(name); 
+        setName("");
+        const inputElement = document.querySelector("input");
+        inputElement.value = ""; 
+    }
+
+    function handleChange(e) {
+        const text = e.target.value; 
+        setName(text);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2 className="label-wrapper">
+                <label htmlFor="new-todo-input" className="label_lg">
+                    What needs to be done? 
+                </label>
+            </h2>
+            <input
+                type="text"
+                id="new-todo-input"
+                className="input input_lg"
+                name="text"
+                autoComplete="off"
+                defaultValue={""}
+                onChange={handleChange}
+            />
+            <button type="submit" className="btn btn_primary btn_lg">
+                Add
+            </button>
+        </form>
+    );
+}
+
+export default Form; 
---
+++ src/components/Todo.js
@@ -0,0 +1,105 @@
+import React, { useState, useRef, useEffect } from "react";  
+import usePrevious from "./UsePrevious";
+
+function Todo(props) {
+    const [isEditing, setEditing] = useState(false); 
+    const [newName, setNewName] = useState("");
+
+    const wasEditing = usePrevious(isEditing); 
+
+    const editFieldRef = useRef(null);
+    const editButtonRef = useRef(null); 
+
+    function handleChange(event)  {
+        const text = event.target.value; 
+        setNewName(text); 
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(`Editing task: "${props.name}" --> "${newName}"`);
+        props.editTask(props.id, newName); 
+        setNewName("");
+        setEditing(false);
+    }
+
+    const editingTemplate = (
+        <form className="stack-small" onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label className="todo-label" htmlFor={props.id}>
+                    New name for {props.name}
+                </label>
+                <input 
+                    id={props.id} 
+                    className="todo-text" 
+                    type="text" 
+                    value={newName}
+                    onChange={handleChange}
+                    ref={editFieldRef}
+                />
+            </div>
+            <div className="btn-group">
+                <button 
+                    type="button" 
+                    className="btn todo-cancel"
+                    onClick={() => setEditing(false)}
+                >
+                    Cancel <span className="visually-hidden">Renaming {props.name}</span>
+                </button>
+                <button type="submit" className="btn btn_primary todo-edit">
+                    Save
+                    <span className="visually-hidden">New name for {props.name}</span>
+                </button>
+            </div>
+        </form>
+    );
+    
+    const viewTemplate = (
+        <div className="stack-small">
+            <div className="c-cb">
+                <input 
+                    id={props.id}
+                    value={props.name}
+                    type="checkbox"
+                    defaultChecked={props.completed}
+                    onChange={() => props.toggleTaskCompleted(props.id)}
+                />
+                <label className="todo-label" htmlFor={props.id}>
+                    {props.name}
+                </label>
+            </div>
+            <div className="btn-group">
+                <button 
+                    type="button" 
+                    className="btn"
+                    onClick={() => setEditing(true)}
+                    ref={editButtonRef}
+                >
+                    Edit
+                    <span className="visually-hidden">{props.name}</span>
+                </button>
+                <button 
+                    type="button"
+                    className="btn btn_danger"
+                    onClick={() => props.deleteTask(props.id)}
+                >
+                    Delete 
+                    <span className="visually-hidden">{props.name}</span>
+                </button>
+            </div>
+        </div>
+    );
+
+    useEffect(() => {
+        if (!wasEditing && isEditing) {
+            editFieldRef.current.focus(); 
+        }
+        if (wasEditing && !isEditing) {
+            editButtonRef.current.focus(); 
+        }
+    }, [wasEditing, isEditing]);
+
+    return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
+}
+
+export default Todo; 
